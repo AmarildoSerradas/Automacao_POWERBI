d974317b15b08cfc89ac1a723d2064ea882a5e7d
AmarildoSerradas <rodriguesamarildo307@gmail.com>
2026-03-22 03:12:21 -0300

    Add files via upload

---
+++ rodarautomacao.bat
@@ -0,0 +1,9 @@
+@echo off
+echo ===== INICIANDO AUTOMACAO =====
+cd /d "C:\Users\TI LOCAL\Documents\projetoBckp\projetos"
+
+"C:\Users\TI LOCAL\Documents\projetoBckp\projetos\venv\Scripts\python.exe" "C:\Users\TI LOCAL\Documents\projetoBckp\projetos\CFC PAR AGUA.py" > log.txt 2>&1
+
+
+echo ===== SCRIPT FINALIZADO =====
+
